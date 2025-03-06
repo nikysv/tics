2ed666d4a1977caf90ed51b147639ff96b8f142c
Kobe Bryan Gonzales Balderrama <kobebgonzalesb@gmail.com>
2025-03-06 08:31:51 -0400

    tu mama123

--- cliente/src/components/register/Registermodal.jsx
+++ cliente/src/components/register/Registermodal.jsx
@@ -22,7 +22,7 @@ const RegisterModal = ({ isOpen, onClose }) => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/register", {
+      const response = await fetch("http://127.0.0.1:5000/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
